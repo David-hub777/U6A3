55126a07cc06367b9f87321e789f36ebb047ee7c
David <david25042001@gmail.com>
2023-12-19 21:00:26 +0100

    cuarto commit

--- src/main/java/com/zetcode/util/GeneratePdfReport.java
+++ src/main/java/com/zetcode/util/GeneratePdfReport.java
@@ -1,5 +1,15 @@
 package com.zetcode.util;
 
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import com.zetcode.model.City;
+
 public class GeneratePdfReport {
+
+	public static ByteArrayInputStream citiesReport(List<City> cities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
