34005032f6bbfb96119e1cbd6fc48b7bf01a1a8b
David <david25042001@gmail.com>
2023-12-19 20:07:11 +0100

    primer commit

---
+++ src/main/java/com/zetcode/U6A3Application.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U6A3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U6A3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zetcode/U6A3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6A3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
